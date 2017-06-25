f0a689e32180ddd23e203fbf89e29a6fa9e6edaf
tejvmade <obsidiankehinde@gmail.com>
2017-06-26 00:28:51 +0100

    Fixes

--- app/src/main/java/com/example/movietwo/MainActivity.java
+++ app/src/main/java/com/example/movietwo/MainActivity.java
@@ -1,41 +1,61 @@
 package com.example.movietwo;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.movietwo.uiux.MainFragment;
+import com.example.movietwo.uiux.SettingsActivity;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final String MAIN_FRAGMENT_TAG = "MFTAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
+
+        if (savedInstanceState != null) {
+        } else {
+
+            addMainFragment();
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu xml; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
             return true;
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void addMainFragment() {
+        if (!isFinishing()) {
+            MainFragment mainFragment = MainFragment.newInstance();
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .add(R.id.main_grid_container, mainFragment, MAIN_FRAGMENT_TAG)
+                    .commit();
+        }
     }
 }
---
+++ app/src/main/java/com/example/movietwo/uiux/MovieGridAdapter.java
@@ -0,0 +1,159 @@
+package com.example.movietwo.uiux;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.movietwo.MainActivity;
+import com.example.movietwo.R;
+import com.example.movietwo.models.Movie;
+
+import java.util.List;
+
+import butterknife.BindView;
+
+public class MovieGridAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+    private static Context mContext;
+    private static List<Movie> mDatasetList;
+
+    private int lastAnimatedItemPosition = -1;
+    private boolean mTwoPane;
+
+
+
+    public MovieGridAdapter(Context context, List<Movie> datasetList, boolean twoPane) {
+        mContext = context;
+        mDatasetList = datasetList;
+        mTwoPane = twoPane;
+    }
+
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder
+            implements View.OnClickListener, View.OnLongClickListener {
+        @BindView(R.id.tv_title)
+        TextView mTVTitle;
+        @BindView(R.id.card_view)
+        CardView mCardView;
+        @BindView(R.id.iv_thumbnail)
+        ImageView mIVThumbNail;
+        @BindView(R.id.tv_rating)
+        TextView mTVRating;
+
+
+        public MovieViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+            this.mIVThumbNail.setOnClickListener(this);
+            view.setOnClickListener(this);
+            view.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            int itemPosition = getAdapterPosition();
+            if (itemPosition != RecyclerView.NO_POSITION) {
+                Movie movie = mDatasetList.get(itemPosition);
+
+                switch (view.getId()) {
+                    case R.id.iv_thumbnail:
+
+                        Bundle bundle = new Bundle();
+                        bundle.putParcelable(ARG_MOVIE_DETAIL, movie);
+
+                        if (mTwoPane) {
+                            addDetailFragmentForTwoPane(bundle);
+                        } else {
+                            Intent movieDetailIntent = new Intent(mContext, MovieDetailActivity.class);
+                            movieDetailIntent.putExtras(bundle);
+                            mContext.startActivity(movieDetailIntent);
+                        }
+                        break;
+
+                    default:
+                        Toast.makeText(mContext, "You clicked at position " + itemPosition +
+                                " on movie thumbnail : " +
+                                movie.getTitle(), Toast.LENGTH_SHORT).show();
+                }
+            }
+
+        }
+
+        public void addDetailFragmentForTwoPane(Bundle bundle) {
+            DetailFragment detailFragment = DetailFragment.newInstance(bundle);
+            FragmentManager fragmentManager = ((MainActivity) mContext).getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .setCustomAnimations(android.support.design.R.anim.abc_grow_fade_in_from_bottom,
+                            android.support.design.R.anim.abc_shrink_fade_out_from_bottom)
+                    .replace(R.id.detail_container, detailFragment, DETAIL_FRAGMENT_TAG)
+                    .commit();
+        }
+
+        @Override
+        public boolean onLongClick(View v) {
+            int itemPosition = getAdapterPosition();
+            return true;
+        }
+    }
+
+
+
+    @Override
+    public MovieViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.movie_item_view, parent, false);
+        return new MovieViewHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        MovieViewHolder cusHolder = (MovieViewHolder) holder;
+        cusHolder.mTVTitle.setText(mDatasetList.get(position).getTitle());
+        cusHolder.mTVRating.setText(String.valueOf(mDatasetList.get(position).getVoteAverage()));
+        String completePosterPath = mDatasetList.get(position).getPosterPath();
+        PabloPicasso.with(mContext).load(completePosterPath).
+                placeholder(R.mipmap.placeholder)
+                .error(R.mipmap.placeholder)
+                .into(cusHolder.mIVThumbNail);
+        cusHolder.mIVThumbNail.setVisibility(View.VISIBLE);
+        setEnterAnimation(cusHolder.mCardView, position);
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return mDatasetList.size();
+    }
+
+    private void setEnterAnimation(View viewToAnimate, int position) {
+
+
+        if (position > lastAnimatedItemPosition) {
+
+
+            Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.translate_up);
+            viewToAnimate.startAnimation(animation);
+            lastAnimatedItemPosition = position;
+        }
+    }
+
+
+    @Override
+    public void onViewDetachedFromWindow(final RecyclerView.ViewHolder holder) {
+        ((MovieViewHolder) holder).mCardView.clearAnimation();
+    }
+}
